b1c3c7fc5e462aecba871ddcb92dc83f35886223
vehcamut <realshuncle@gmail.com>
2023-06-25 13:42:19 +0400

    added layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+import Header from "../Header/Header";
+import classes from './layout.module.scss';
+import Footer from "../Footer/Footer";
+import { Roboto } from "next/font/google";
+import classNames from "classnames";
+
+const roboto = Roboto(
+  {
+    weight: ['400', '700'],
+    subsets: ['cyrillic', 'latin'],
+    style: ['normal', 'italic'],
+  }
+);
+
+
+const Layout:FunctionComponent<PropsWithChildren>  = ({ children }) =>  {
+  return (
+    <>
+      <div className="modal-container"/>
+      <div className={classNames(classes['container'], roboto.className)}>
+        <Header />
+        <main className={classes['main']}>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Layout;
