e0ed8db1f4f1808ed5aaeae9c9991318ddd8b5eb
vehcamut <realshuncle@gmail.com>
2023-06-25 18:54:58 +0400

    minor environment changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        //pathname: '/account123/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/movies',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
