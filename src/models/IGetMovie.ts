69b243832b4d5aa2b02ab5ffe0f9d6db84a28507
vehcamut <realshuncle@gmail.com>
2023-06-25 13:43:39 +0400

    added some models

---
+++ src/models/IGetMovie.ts
@@ -0,0 +1,3 @@
+export interface IGetMovie {
+  movieId?: string;
+}
---
+++ src/models/IMovie.ts
@@ -0,0 +1,11 @@
+export interface IMovie {
+  title: string;
+  posterUrl: string;
+  releaseYear: number;
+  description: string;
+  genre: string;
+  id: string;
+  rating: number;
+  director: string;
+  reviewIds: string[];
+}
